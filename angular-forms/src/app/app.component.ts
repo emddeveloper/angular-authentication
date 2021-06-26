728f5d1664b6dabcd5e5ffc56ce264965d05e96a
emddeveloper <emd.developer@gmail.com>
2021-06-26 10:38:32 +0530

    Template driven form setup done

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-forms';
+  @ViewChild('f') loginformvalue: NgForm;
+  onSubmit(formvalue: NgForm) {
+    console.log(formvalue.value);
+    console.log(this.loginformvalue.value);
+  }
 }
