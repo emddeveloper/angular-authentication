ec6cff2a4a54e3ef16b5cd5bf23255daa0eaffee
emddeveloper <emd.developer@gmail.com>
2021-06-26 12:09:32 +0530

    radio button and setValue and patchValue done with @viewChild

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -9,9 +9,22 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'angular-forms';
   defaultSecret = 'pet';
+  data = '';
+  genders = ['male', 'female'];
+  suggestUsername = '';
   @ViewChild('f') loginformvalue: NgForm;
   onSubmit(formvalue: NgForm) {
     console.log(formvalue.value);
+    this.data = this.loginformvalue.value;
     console.log(this.loginformvalue.value);
+  }
+  suggestname() {
+    let random = Math.floor(Math.random() * 100);
+    this.suggestUsername = 'emddeveloper_' + random;
+    this.loginformvalue.form.patchValue({
+      userData: {
+        username: this.suggestUsername,
+      },
+    });
   }
 }
